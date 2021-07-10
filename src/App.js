08327d976afaec5920433e681221f6554e7c3131
Ally4 <el.ally741@gmail.com>
2021-07-10 02:53:52 +0200

    feature(works): all the remaining works is in this repository [finishes]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState } from 'react';
+import axios from 'axios';
+import SearchBar from './components/SearchBar/Searchbar';
+import Photos from './components/Photos/Photos';
+import validate from './validation/validateInput';
 
-function App() {
+const App = () => {
+  const [textMessage, setMessage] = useState("");
+  const [photos, setPhotos] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [userInput, setInput] = useState({
+    value: "",
+    validation: {
+      required: true,
+      isNumber: true,
+    },
+    valid: false,
+    touched: false,
+  });
+
+  const inputHandler = (e) => {
+    const updatedUserInput = {
+      ...userInput,
+      value: e.target.value,
+      valid: validate(e.target.value, userInput.validation),
+      touched: true,
+    };
+
+    setInput(updatedUserInput);
+  };
+
+  const handleClick = async () => {
+    const albumId = userInput.value;
+    setLoading(true);
+
+    try {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/albums/${albumId}/photos`
+      );
+
+	  if(response.data.length === 0) setMessage("no album");
+
+      setPhotos(response.data);
+      setLoading(false);
+    } catch (err) {
+      setMessage(
+        "There might be a problem with your internet"
+      );
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+
+      <div className="container">
+        <h1 className="pres">The Album photo</h1>
+        <SearchBar value={userInput.value} inputValid={userInput.valid} onchange={inputHandler} onclick={handleClick} />
+        {isLoading ? <h4 className="pres">Its Loading ...</h4> : <Photos photos={photos} />}
+        {!isLoading && !photos.length > 0 && textMessage && <h2 className="text-center">{textMessage}</h2>}
+      </div>
+	  
+    </Fragment>
   );
-}
+};
 
 export default App;
---
+++ src/validation/validateInput.js
@@ -0,0 +1,21 @@
+const validate= ( value, rules ) => {
+	let isValid = true;
+
+	if ( ! rules ) {
+		return true;
+	}
+
+	if ( rules.required ) {
+		isValid = value.trim() !== '' && isValid;
+	}
+
+	if ( rules.isNumber ) {
+		const pattern = /^[0-9]+$/;
+		isValid = pattern.test( value ) && isValid;
+	}
+
+	return isValid;
+};
+
+
+export default validate;
